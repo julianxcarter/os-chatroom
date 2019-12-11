6b09fdc2e558321f7095739eaa38ddab8cb3f8aa
julianxcarter <juliancarter55@gmail.com>
2019-12-11 11:22:49 -0500

    New changes

--- src/server.py
+++ src/server.py
@@ -2,17 +2,9 @@ import socket
 import select 
 import sys 
 from _thread import *
-  
-"""The first argument AF_INET is the address domain of the 
-socket. This is used when we have an Internet Domain with 
-any two hosts. The second argument is the type of socket. 
-SOCK_STREAM means that data or characters are read in 
-a continuous flow."""
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
-  
-  
 
 IP_address = "0.0.0.0"
 Port = 1278
